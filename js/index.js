74dda9e9cf496bd18854f7c9de71534d0ead6105
unknown <Humairasayyed00786@gmail.com>
2021-01-10 19:31:33 +0530

    added spinner

--- js/index.js
+++ js/index.js
@@ -1,15 +1,16 @@
 $(function() {
     $("#getQuote").click(fetchQuote)
-
 })
 function fetchQuote() {
-     $.get({
+    $('.modal').modal('show') 
+    $.get({
             url: "https://api.quotable.io/random",
             dataType: "json",
             success:function(data){
              const {author, content} = data
                 $(".quote").html(content)
                  $(".author").html(`--- ${author}`)
+                 $('.modal').modal('hide') 
             }
         });
 }
